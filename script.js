a14aa3bbefdacd8fbfb2a17a845f9ddf3ba88b4e
dima_begalin <admin@zerek.com>
2020-01-25 21:57:27 +0600

    add homepage and her style

---
+++ script.js
@@ -0,0 +1,15 @@
+var colorArray = ["#b96565", "#fff", "#000"];
+var i = 0;
+
+function changeColor() {
+    document.body.style.background = colorArray[i];
+    i++;
+    if(i > colorArray.length - 1) {
+        i = 0;
+    }
+    alert("Hello, World!")
+}
+
+function visibil() {
+    document.d
+}
